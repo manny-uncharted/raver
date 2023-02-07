42ab6e564b50d1a38b0d29954be9f8c365b503d5
manny-uncharted <omoebun52@gmail.com>
2023-02-07 20:58:13 +0100

    wrote the dashboard url and settings for the redirect url, login and logout url

--- raver/account/urls.py
+++ raver/account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # Login / Logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('', views.dashboard, name='dashboard'),
 ]
