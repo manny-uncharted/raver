05b3bd8524d37847e0c3cd101f8e1c7a20fc43ac
manny-uncharted <omoebun52@gmail.com>
2023-02-08 19:52:37 +0100

    implemented the password reset

--- raver/account/urls.py
+++ raver/account/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
     path('', views.dashboard, name='dashboard'),
 ]
