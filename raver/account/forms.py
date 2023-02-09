76e7a90ceb222592f89644734fefe1ca1b2e6f2a
manny-uncharted <omoebun52@gmail.com>
2023-02-09 16:47:05 +0100

    implemented user registration form

--- raver/account/forms.py
+++ raver/account/forms.py
@@ -3,7 +3,25 @@ This is a login form to allow users to login to the site.
 """
 
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+        widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        clean_data = self.cleaned_data
+        if clean_data['password'] != clean_data['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return clean_data['password2']
+        
