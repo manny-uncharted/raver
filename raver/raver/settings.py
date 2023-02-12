93c7a77dbd4f0a398483c67c94803d904d3c4252
manny-uncharted <omoebun52@gmail.com>
2023-02-13 00:06:14 +0100

    reimplemented the registration form

--- raver/raver/settings.py
+++ raver/raver/settings.py
@@ -103,6 +103,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Password Hashers
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
